a8a1f92a17229fdc53396f95b473c123a102b146
devbilalashraf <devbilal.ashraf786@gmail.com>
2024-04-19 01:30:46 +0500

    Handle carousel based on the screen size

--- client/src/pages/testimonial.tsx
+++ client/src/pages/testimonial.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from "react";
+import { FC, useEffect, useMemo, useState } from "react";
 import { Box, Button, Card, CardContent, Container, Typography, styled } from "@mui/material";
 import { Rating } from "@mui/material";
 import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
@@ -40,139 +40,327 @@ import "swiper/swiper-bundle.css";
 export const Testimonial: FC = () => {
   const [isEnd, setIsEnd] = useState(false);
   const [isStart, setIsStart] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup function to remove the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   const handleSlideChange = (swiper: any) => {
     setIsEnd(swiper.isEnd);
     setIsStart(swiper.isBeginning)
   };
-
+  
   return (
     <StyledBox>
       <StyledBackgroundBoxTestimonial  sx={{ backgroundImage: "url('../../src/assets/Rectangle 1378.png')" }}>
-      <Box className="content-box" sx={{ padding: "30px" }}>
-      <h4 className="sub-heading-text">
-        <span className="line-span-holder">
-          <span className="line-span"/>
-          <span className="line-span"/>
-        </span>
+        <Box className="content-box" sx={{ padding: "30px" }}>
+          <h4 className="sub-heading-text">
+            <span className="line-span-holder">
+              <span className="line-span"/>
+              <span className="line-span"/>
+            </span>
         TESTIMONIAL
-      </h4>
-      <Box className="content-holder">
-        <h1 className="heading-text">Build Everything You Needs</h1>
-      </Box>
-
-      <Box>
-        <Swiper
-          modules={[Navigation, Pagination, A11y]}
-          slidesPerView="auto"
-          spaceBetween={50}
-          onSlideChange={(swiper) => handleSlideChange(swiper)}
-        >
-          <SwiperSlide  >
-            <Box sx={{ display: "flex", justifyContent: "center" }}>
-              <Container>
-                <Card sx={{ marginTop: "30px", maxWidth: 345, position: "relative", marginBottom: 2, overflow: "visible" }}>
-                  <img
-                    src="../../src/assets/Rectangle 48.jpg"
-                    alt="Lorem Ipsum" style={{ position: "absolute", top: -30, right: 20 }}
-                  />
-                  <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                      {"Lorem Ipsum"}
-                    </Typography>
-                    <Typography gutterBottom variant="h6" component="div">
-                      {"Lorem Ipsum"}
-                    </Typography>
-                    <Rating name="read-only" value={4} readOnly />
-                    <Typography variant="body2" color="text.secondary">
-                      {
-                        `Lorem Ipsum is simply dummy text of the printing and 
+          </h4>
+          <Box className="content-holder">
+            <h1 className="heading-text">Build Everything You Needs</h1>
+          </Box>
+          <Box>
+            <Swiper
+              breakpoints={{
+                480: {
+                  slidesPerView: 1,
+                  spaceBetween: 40,
+                },
+                768: {
+                  slidesPerView: 2,
+                  spaceBetween: 40,
+                },
+              }}
+              modules={[Navigation, Pagination, A11y]}
+              onSlideChange={(swiper: any) => handleSlideChange(swiper)}
+              pagination={windowWidth < 768 ? { clickable: true } : false}
+            >
+              <SwiperSlide>
+                <Box sx={{ display: "flex", justifyContent: "center" }}>
+                  <Container>
+                    <Card 
+                      sx={{ 
+                        marginTop: "30px",
+                        maxWidth: 345,
+                        position: "relative",
+                        marginBottom: 2,
+                        overflow: "visible" 
+                      }}
+                    >
+                      <img
+                        src="../../src/assets/Rectangle 48.jpg"
+                        alt="Lorem Ipsum" style={{ position: "absolute", top: -30, right: 20 }}
+                      />
+                      <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                          {"Lorem Ipsum"}
+                        </Typography>
+                        <Typography gutterBottom variant="h6" component="div">
+                          {"Lorem Ipsum"}
+                        </Typography>
+                        <Rating name="read-only" value={4} readOnly />
+                        <Typography variant="body2" color="text.secondary">
+                          {
+                            `Lorem Ipsum is simply dummy text of the printing and 
                       typesetting  industry. Lorem Ipsum has been.`
-                      }
-                    </Typography>
-                  </CardContent>
-                </Card>
-              </Container>
-              <Container>
-                <Card sx={{ marginTop: "30px", maxWidth: 345, position: "relative", marginBottom: 2, overflow: "visible" }}>
-                  <img
-                    src="../../src/assets/Rectangle 48.jpg"
-                    alt="Lorem Ipsum" style={{ position: "absolute", top: -30, right: 20 }}
-                  />
-                  <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                      {"Lorem Ipsum"}
-                    </Typography>
-                    <Typography gutterBottom variant="h6" component="div">
-                      {"Lorem Ipsum"}
-                    </Typography>
-                    <Rating name="read-only" value={5} readOnly />
-                    <Typography variant="body2" color="text.secondary">
-                      {
-                        `Lorem Ipsum is simply dummy text of the printing and 
+                          }
+                        </Typography>
+                      </CardContent>
+                    </Card>
+                  </Container>
+                  <Container>
+                    <Card
+                      sx={{ 
+                        marginTop: "30px",
+                        maxWidth: 345,
+                        position: "relative",
+                        marginBottom: 2,
+                        overflow: "visible" 
+                      }}
+                    >
+                      <img
+                        src="../../src/assets/Rectangle 48.jpg"
+                        alt="Lorem Ipsum" style={{ position: "absolute", top: -30, right: 20 }}
+                      />
+                      <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                          {"Lorem Ipsum"}
+                        </Typography>
+                        <Typography gutterBottom variant="h6" component="div">
+                          {"Lorem Ipsum"}
+                        </Typography>
+                        <Rating name="read-only" value={5} readOnly />
+                        <Typography variant="body2" color="text.secondary">
+                          {
+                            `Lorem Ipsum is simply dummy text of the printing and 
                       typesetting  industry. Lorem Ipsum has been.`
-                      }
-                    </Typography>
-                  </CardContent>
-                </Card>
-              </Container>
-            </Box>
-          </SwiperSlide>
-          <SwiperSlide >
-            <Box sx={{ display: "flex", justifyContent: "center" }}>
-              <Container>
-                <Card sx={{ maxWidth: 345, position: "relative", marginBottom: 2, overflow: "visible" }}>
-                  <img
-                    src="../../src/assets/Rectangle 48.jpg"
-                    alt="Lorem Ipsum" style={{ position: "absolute", top: -30, right: 20 }}
-                  />
-                  <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                      {"Lorem Ipsum"}
-                    </Typography>
-                    <Typography gutterBottom variant="h6" component="div">
-                      {"Lorem Ipsum"}
-                    </Typography>
-                    <Rating name="read-only" value={4} readOnly />
-                    <Typography variant="body2" color="text.secondary">
-                      {
-                        `Lorem Ipsum is simply dummy text of the printing and 
+                          }
+                        </Typography>
+                      </CardContent>
+                    </Card>
+                  </Container>
+                </Box>
+              </SwiperSlide>
+              <SwiperSlide>
+                <Box sx={{ display: "flex", justifyContent: "center" }}>
+                  <Container>
+                    <Card 
+                      sx={{ 
+                        marginTop: "30px",
+                        maxWidth: 345,
+                        position: "relative",
+                        marginBottom: 2,
+                        overflow: "visible" 
+                      }}
+                    >
+                      <img
+                        src="../../src/assets/Rectangle 48.jpg"
+                        alt="Lorem Ipsum" style={{ position: "absolute", top: -30, right: 20 }}
+                      />
+                      <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                          {"Lorem Ipsum"}
+                        </Typography>
+                        <Typography gutterBottom variant="h6" component="div">
+                          {"Lorem Ipsum"}
+                        </Typography>
+                        <Rating name="read-only" value={4} readOnly />
+                        <Typography variant="body2" color="text.secondary">
+                          {
+                            `Lorem Ipsum is simply dummy text of the printing and 
                       typesetting  industry. Lorem Ipsum has been.`
-                      }
-                    </Typography>
-                  </CardContent>
-                </Card>
-              </Container>
-              <Container>
-                <Card sx={{ maxWidth: 345, position: "relative", marginBottom: 2, overflow: "visible" }}>
-                  <img
-                    src="../../src/assets/Rectangle 48.jpg"
-                    alt="Lorem Ipsum" style={{ position: "absolute", top: -30, right: 20 }}
-                  />
-                  <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                      {"Lorem Ipsum"}
-                    </Typography>
-                    <Typography gutterBottom variant="h6" component="div">
-                      {"Lorem Ipsum"}
-                    </Typography>
-                    <Rating name="read-only" value={5} readOnly />
-                    <Typography variant="body2" color="text.secondary">
-                      {
-                        `Lorem Ipsum is simply dummy text of the printing and 
+                          }
+                        </Typography>
+                      </CardContent>
+                    </Card>
+                  </Container>
+                  <Container>
+                    <Card
+                      sx={{ 
+                        marginTop: "30px",
+                        maxWidth: 345,
+                        position: "relative",
+                        marginBottom: 2,
+                        overflow: "visible" 
+                      }} 
+                    >
+                      <img
+                        src="../../src/assets/Rectangle 48.jpg"
+                        alt="Lorem Ipsum" style={{ position: "absolute", top: -30, right: 20 }}
+                      />
+                      <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                          {"Lorem Ipsum"}
+                        </Typography>
+                        <Typography gutterBottom variant="h6" component="div">
+                          {"Lorem Ipsum"}
+                        </Typography>
+                        <Rating name="read-only" value={5} readOnly />
+                        <Typography variant="body2" color="text.secondary">
+                          {
+                            `Lorem Ipsum is simply dummy text of the printing and 
                       typesetting  industry. Lorem Ipsum has been.`
-                      }
-                    </Typography>
-                  </CardContent>
-                </Card>
-              </Container>
-            </Box>
-          </SwiperSlide>
-
-        </Swiper>
-      </Box>
-      </Box>
-    </StyledBackgroundBoxTestimonial>
+                          }
+                        </Typography>
+                      </CardContent>
+                    </Card>
+                  </Container>
+                </Box>
+              </SwiperSlide>
+              <SwiperSlide>
+                <Box sx={{ display: "flex", justifyContent: "center" }}>
+                  <Container>
+                    <Card 
+                      sx={{ 
+                        marginTop: "30px",
+                        maxWidth: 345,
+                        position: "relative",
+                        marginBottom: 2,
+                        overflow: "visible" 
+                      }}
+                    >
+                      <img
+                        src="../../src/assets/Rectangle 48.jpg"
+                        alt="Lorem Ipsum" style={{ position: "absolute", top: -30, right: 20 }}
+                      />
+                      <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                          {"Lorem Ipsum"}
+                        </Typography>
+                        <Typography gutterBottom variant="h6" component="div">
+                          {"Lorem Ipsum"}
+                        </Typography>
+                        <Rating name="read-only" value={4} readOnly />
+                        <Typography variant="body2" color="text.secondary">
+                          {
+                            `Lorem Ipsum is simply dummy text of the printing and 
+                      typesetting  industry. Lorem Ipsum has been.`
+                          }
+                        </Typography>
+                      </CardContent>
+                    </Card>
+                  </Container>
+                  <Container>
+                    <Card
+                      sx={{ 
+                        marginTop: "30px",
+                        maxWidth: 345,
+                        position: "relative",
+                        marginBottom: 2,
+                        overflow: "visible" 
+                      }} 
+                    >
+                      <img
+                        src="../../src/assets/Rectangle 48.jpg"
+                        alt="Lorem Ipsum" style={{ position: "absolute", top: -30, right: 20 }}
+                      />
+                      <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                          {"Lorem Ipsum"}
+                        </Typography>
+                        <Typography gutterBottom variant="h6" component="div">
+                          {"Lorem Ipsum"}
+                        </Typography>
+                        <Rating name="read-only" value={5} readOnly />
+                        <Typography variant="body2" color="text.secondary">
+                          {
+                            `Lorem Ipsum is simply dummy text of the printing and 
+                      typesetting  industry. Lorem Ipsum has been.`
+                          }
+                        </Typography>
+                      </CardContent>
+                    </Card>
+                  </Container>
+                </Box>
+              </SwiperSlide>
+              <SwiperSlide>
+                <Box sx={{ display: "flex", justifyContent: "center" }}>
+                  <Container>
+                    <Card 
+                      sx={{ 
+                        marginTop: "30px",
+                        maxWidth: 345,
+                        position: "relative",
+                        marginBottom: 2,
+                        overflow: "visible" 
+                      }}
+                    >
+                      <img
+                        src="../../src/assets/Rectangle 48.jpg"
+                        alt="Lorem Ipsum" style={{ position: "absolute", top: -30, right: 20 }}
+                      />
+                      <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                          {"Lorem Ipsum"}
+                        </Typography>
+                        <Typography gutterBottom variant="h6" component="div">
+                          {"Lorem Ipsum"}
+                        </Typography>
+                        <Rating name="read-only" value={4} readOnly />
+                        <Typography variant="body2" color="text.secondary">
+                          {
+                            `Lorem Ipsum is simply dummy text of the printing and 
+                      typesetting  industry. Lorem Ipsum has been.`
+                          }
+                        </Typography>
+                      </CardContent>
+                    </Card>
+                  </Container>
+                  <Container>
+                    <Card
+                      sx={{ 
+                        marginTop: "30px",
+                        maxWidth: 345,
+                        position: "relative",
+                        marginBottom: 2,
+                        overflow: "visible" 
+                      }} 
+                    >
+                      <img
+                        src="../../src/assets/Rectangle 48.jpg"
+                        alt="Lorem Ipsum" style={{ position: "absolute", top: -30, right: 20 }}
+                      />
+                      <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                          {"Lorem Ipsum"}
+                        </Typography>
+                        <Typography gutterBottom variant="h6" component="div">
+                          {"Lorem Ipsum"}
+                        </Typography>
+                        <Rating name="read-only" value={5} readOnly />
+                        <Typography variant="body2" color="text.secondary">
+                          {
+                            `Lorem Ipsum is simply dummy text of the printing and 
+                      typesetting  industry. Lorem Ipsum has been.`
+                          }
+                        </Typography>
+                      </CardContent>
+                    </Card>
+                  </Container>
+                </Box>
+              </SwiperSlide>
+              {
+                windowWidth > 768 && 
+               <SwiperButtonNext isEnd={isEnd} isStart={isStart}/>
+              }
+            </Swiper>
+          </Box>
+        </Box>
+      </StyledBackgroundBoxTestimonial>
     </StyledBox>
   );
 };
